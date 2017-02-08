2078c7b9aca5cf10db4431cf3ba7c78710edc66a
poppycompass <t0g0v31dk@gmail.com>
2017-02-08 19:21:05 +0900

    add commands for Mac

--- start.sh
+++ start.sh
@@ -33,6 +33,13 @@ if uname -a | grep ARCH >/dev/null && [ ! -d /bin/zsh ]; then
   fi
 fi
 
+# for Mac
+if uname -a | grep "Darwin Kernel" >/dev/nlll && [ ! -d /bin/zsh ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install neovim/neovim/neovim
+  brew install zsh git binutils gdb
+fi
+
 if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
     echo "You don't have Neobundle. Install now..."
     mkdir -p ~/.vim/bundle
