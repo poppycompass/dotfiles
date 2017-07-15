07d55c08acf3aabd88985102855340e2614c4ef1
poppycompass <t0g0v31dk@gmail.com>
2017-07-15 14:28:51 +0900

    fix bug

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ if uname -a | grep ARCH >/dev/null && [ ! -d /bin/zsh ]; then
 fi
 
 # for Mac
-if uname -a | grep "Darwin Kernel" >/dev/nlll && [ ! -d /bin/zsh ]; then
+if uname -a | grep "Darwin Kernel" >/dev/null && [ ! -d /bin/zsh ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew install neovim/neovim/neovim
   brew install zsh git binutils gdb
