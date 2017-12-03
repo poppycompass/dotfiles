5e686dbec949cda49d097e43e056c9e012a1239e
poppycompass <t0g0v31dk@gmail.com>
2017-12-03 18:22:15 +0900

    add gnu global install

--- setup/arch.sh
+++ setup/arch.sh
@@ -1,8 +1,23 @@
 # setup script for Arch linux
 sudo pacman -Sy
-sudo pacman -S git neovim zsh radare2 gdb python-neovim
+sudo pacman -S git neovim zsh radare2 gdb python-neovim ctags
 if [ "$1" = "x" ]; then
   sudo pacman -S xmonad xmobar rxvt-unicode gmrun dmenu thunderbird firefox
   yaourt -S google-chrome
 fi
+
 ln -sf ~/dotfiles/.Xmodmap.arch ~/.Xmodmap
+
+# install global
+if [ ! -d /usr/local/global ]; then
+  cd /usr/local/src
+  sudo wget http://tamacom.com/global/global-6.5.7.tar.gz
+  sudo tar xvf global-6.5.7.tar.gz
+  sudo ./configure --prefix=/usr/local/global
+  sudo make
+  sudo make instll
+  sudo ln -s /usr/local/global/bin/gtags /usr/bin/gtags
+  sudo ln -s /usr/local/global/bin/global /usr/bin/global
+  sudo ln -s /usr/local/global/bin/htags /usr/bin/htags
+  sudo ln -s /usr/local/global/bin/htags-server /usr/bin/htags-server
+fi
--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -3,8 +3,8 @@ sudo apt-get -y install software-properties-common # for neovim
 sudo apt-get -y install python-dev python-pip python3-dev python3-pip # for neovim
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get -y update
-sudo apt-get -y install neovim vim gcc gdb
-sudo pip install neovim
+sudo apt-get -y install neovim vim gcc gdb ctags
+sudo pip3 install neovim
 sudo apt-get -y install zsh git
 if [ "$1" = "x" ]; then
   sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -14,3 +14,17 @@ if [ "$1" = "x" ]; then
   sudo apt-get -y install xmobar xmonad rxvt-unicode-256color gmrun suckless-tools
 fi
 ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
+
+# install global
+if [ ! -d /usr/local/global ]; then
+  cd /usr/local/src
+  sudo wget http://tamacom.com/global/global-6.5.7.tar.gz
+  sudo tar xvf global-6.5.7.tar.gz
+  sudo ./configure --prefix=/usr/local/global
+  sudo make
+  sudo make instll
+  sudo ln -s /usr/local/global/bin/gtags /usr/bin/gtags
+  sudo ln -s /usr/local/global/bin/global /usr/bin/global
+  sudo ln -s /usr/local/global/bin/htags /usr/bin/htags
+  sudo ln -s /usr/local/global/bin/htags-server /usr/bin/htags-server
+fi
