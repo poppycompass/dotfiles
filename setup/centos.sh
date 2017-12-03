ea0173f5715e2681cc8fe719575844604264a3ab
poppycompass <t0g0v31dk@gmail.com>
2017-12-03 18:51:03 +0900

    fix setup centos.sh

--- setup/centos.sh
+++ setup/centos.sh
@@ -1,2 +1,8 @@
 # setup script for CentOS
+yum -y install epel-release
+curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo 
+yum -y install neovim vim gcc gdb ctags
+yum -y install zsh git
+sudo pip3 install neovim
+
 ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
