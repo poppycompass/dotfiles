2a2b35ab88d752acb2e36e3bfc25555e56a33434
poppycompass <t0g0v31dk@gmail.com>
2019-07-02 23:10:00 +0900

    add special settings for thinkpad

---
+++ setup/thinkpad.sh
@@ -0,0 +1,7 @@
+# All you need is thinkpad.
+
+#xinput --list --short
+#xinput --list-props <id>
+
+xinput --set-prop 12 331 0.9 # 0~1.0, make mouse pointer fast
+xinput --disable 11 # disable touchpad
