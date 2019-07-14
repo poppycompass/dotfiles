021c8310f30237e5d8d98589f25d47978957542c
poppycompass <t0g0v31dk@gmail.com>
2019-07-14 11:57:16 +0900

    fix: mouse speed script

--- setup/thinkpad.sh
+++ setup/thinkpad.sh
@@ -3,5 +3,10 @@
 #xinput --list --short
 #xinput --list-props <id>
 
-xinput --set-prop 12 331 0.9 # 0~1.0, make mouse pointer fast
-xinput --disable 11 # disable touchpad
+SPEED=1.0
+TrackPointID=`xinput --list --short | perl -ne 'if ($_=~/TrackPoint/) {s/.*id=(\d+).*/$1/; print $_}'`
+TrackPointAS=`xinput --list-props ${TrackPointID} | perl -ne 'if ($_=~/Accel Speed \(/) {s/.*\((.+?)\).*/$1/; print $_}'`
+xinput --set-prop ${TrackPointID} ${TrackPointAS} ${SPEED} # 0~1.0, make mouse pointer fast
+
+TouchPadID=`xinput --list --short | perl -ne 'if ($_=~/TouchPad/) {s/.*id=(\d+).*/$1/; print $_}'`
+xinput --disable ${TouchPadID} # disable touchpad
