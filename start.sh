8015edbc166144c37e03edf268f4fc1b0a04a7fc
poppycompass <t0g0v31dk@gmail.com>
2020-01-16 23:01:24 +0900

    add neosnippet settings & create vim/snippets

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# http://patorjk.com/software/taag/#p=display&f=Blocks&t=poppycompass, font: Blocks, Doh, Doom, Ogre, Slant, Larry 3D is good, now is Slant, 
+# http://patorjk.com/software/taag/#p=display&f=Blocks&t=poppycompass, font: Blocks, Doh, Doom, Ogre, Slant, Larry 3D is good, now is Slant,
 /bin/echo -e "\e[36;1;7m
 ______                                                                                   ______
 \ \ \ \     ____  ____  ____  ____  __  ___________  ____ ___  ____  ____ ___________   / / / /
- \ \ \ \   / __ \/ __ \/ __ \/ __ \/ / / / ___/ __ \/ __ \`__ \/ __ \/ __ \`/ ___/ ___/  / / / / 
- / / / /  / /_/ / /_/ / /_/ / /_/ / /_/ / /__/ /_/ / / / / / / /_/ / /_/ (__  |__  )   \ \ \ \ 
+ \ \ \ \   / __ \/ __ \/ __ \/ __ \/ / / / ___/ __ \/ __ \`__ \/ __ \/ __ \`/ ___/ ___/  / / / /
+ / / / /  / /_/ / /_/ / /_/ / /_/ / /_/ / /__/ /_/ / / / / / / /_/ / /_/ (__  |__  )   \ \ \ \
 /_/_/_/  / .___/\____/ .___/ .___/\__, /\___/\____/_/ /_/ /_/ .___/\__,_/____/____/     \_\_\_\\
-        /_/         /_/   /_/    /____/                    /_/                                 
+        /_/         /_/   /_/    /____/                    /_/
 \e[m\n\n"
 
 SETUP_DIR="setup"
@@ -61,6 +61,7 @@ mkdir -p ~/.vim/bundle
 ln -sf ~/dotfiles/vim/after ~/.vim/
 ln -sf ~/dotfiles/vim/colors ~/.vim/
 ln -sf ~/dotfiles/vim/userautoload ~/.vim/
+ln -sf ~/dotfiles/vim/snippets ~/.vim/
 ln -sf ~/.vim ~/dotfiles/nvim
 ln -sf ~/dotfiles/.vimrc ~/dotfiles/nvim/init.vim
 ln -sf ~/dotfiles/xmonad.hs ~/.xmonad/xmonad.hs
