cf00b2bbfb10c7b33dfca96fad49cee29e0d31c1
poppycompass <t0g0v31dk@gmail.com>
2020-02-05 11:20:16 +0900

    refix version checking

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,22 +1,22 @@
 # setup script for ubuntu/debian
 
 VERSION=`cat /etc/os-release | grep "^VERSION_ID=" | cut -d'=' -f2 | perl -pe 's/\n//g'`
-BASIC="zsh git vim gcc gdb ctags libncurses5-dev libboost-all-dev cmake"
+BASIC="zsh git vim gcc gdb ctags libncurses5-dev libboost-all-dev cmake wget"
 NEOVIM="neovim python-dev python-pip python3-dev python3-pip software-properties-common"
 XMONAD="xmobar xmonad rxvt-unicode-256color gmrun suckless-tools chromium-browser"
 
 echo "[+] OS version is ${VERSION}"
 case ${VERSION} in
-  "12.04") sudo apt-get update && \
-           sudo apt-get install ${NEOVIM} ${BASIC} ;;
-  "14.04" | \
-  "16.04") sudo add-apt-repository -y ppa:neovim-ppa/unstable && \
-           sudo apt update && \
-           sudo apt install ${NEOVIM} ${BASIC} && \
-           sudo pip3 install neovim neovim-remote ;;
-  "18.04") sudo apt update && \
-           sudo apt install ${NEOVIM} ${BASIC} && \
-           sudo pip3 install neovim neovim-remote ;;
+  *12.04* ) sudo apt-get update && \
+            sudo apt-get install ${NEOVIM} ${BASIC} ;;
+  *14.04* | \
+  *16.04* ) sudo add-apt-repository -y ppa:neovim-ppa/unstable && \
+            sudo apt update && \
+            sudo apt install ${NEOVIM} ${BASIC} && \
+            sudo pip3 install neovim neovim-remote ;;
+  *18.04* ) sudo apt update && \
+            sudo apt install ${NEOVIM} ${BASIC} && \
+            sudo pip3 install neovim neovim-remote ;;
   * ) echo "[-] WTF?! OS version not found." ;;
 esac
 
